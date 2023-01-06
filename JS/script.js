5be2cdf65898478b5532366a31bd2776b260104b
yodit <yoditabebe9391gmail.com>
2023-01-06 17:00:17 +0300

    Add modified html, css and JS files for contact validation

--- JS/script.js
+++ JS/script.js
@@ -316,19 +316,20 @@ seeProject.forEach((links) => {
 });
 
 const submitButton = document.querySelector('#submit-button');
-const errorMsg = document.querySelector('.span');
 function validate() {
-  const inputEmail = document.getElementById('email');
-  const inpuEmailText = inputEmail.value;
-  const validator = /[^a-z]/;
-  document.forms[1].onsubmit = function email(e) {
-    if (!(validator.test(inpuEmailText))) {
-      errorMsg.className('error-msg');
+  const text = document.getElementById('email').value;
+  const validator = /^([a-z\d-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+  document.forms[0].onsubmit = function email(e) {
+    if (!(validator.test(text))) {
+      document.getElementById('error-msg').innerHTML = 'Please use lowercase to enter valid email address!';
+      document.getElementById('error-msg').style.visibility = 'visible';
+      document.getElementById('error-msg').style.color = 'white';
+      document.getElementById('error-msg').style.background = 'red';
+      document.getElementById('error-msg').style.textAlign = 'center';
       e.preventDefault();
     }
   };
 }
-
 submitButton.addEventListener('click', () => {
-  validateFun();
+  validate();
 });
